8637609ed9b3c6ddc7e32248f7fd9e27aa7a441b
antisochelle <delarosamchelle@gmail.com>
2016-12-10 18:37:28 +0000

    added action.php

---
+++ action.php
@@ -0,0 +1,2 @@
+Hi <?php echo htmlspecialchars($_POST['userID']); ?>.
+Your password is <?php echo (int)$_POST['userPassword']; ?> years old.
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <title>CS 165 APP</title>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -21,13 +21,38 @@
 		display: block;
 	}
 
-	.logo {
+	a.logo {
 		font-size: 25px;
 		color: white;
 		text-decoration: none;
 		margin: auto;
 	}
 
+	.login {
+		margin-top: 7px;
+		margin-right: 10px;
+		margin-bottom: 0px;
+		float: right;
+	}
+
+	.textbox {
+		font-family: "Raleway", sans-serif;
+		margin-right: 10px;
+		margin-top: 3px;
+		float: right;
+	}
+
+	.submit {
+		font-family: "Montserrat", sans-serif;
+		font-size: 15px;
+		background-color: white;
+		color: black;
+		height: 21.5px;
+		min-width: 50px;
+		border: 0;
+		margin-top: 3px;
+		float: right;
+	}
 
 	.mainbody {
 		margin-left: 50px;
@@ -41,11 +66,20 @@
 	<!--HEADER-->
 	<div class="top" style="position: fixed; top: 0px; left: 0px; right: 0px;">
 		<a href="https://cs165.herokuapp.com/" class="logo">B E S H I E</a>
+
+		<form action="action.php" method="post" class="textbox">
+		<input class="submit" type="submit" value="SIGN UP" style="margin-right: 30px;">
+		<input class="submit" type="submit" value="Shop Now" style="margin-right: 10px;">
+		<input class="textbox" type="password" name="userPassword" placeholder="Password">
+		<input class="textbox" type="text" name="userID" placeholder="Username" maxlength="50">
+		</form>
+		<p class="login">Login:</p>
+		
 	</div>
 
 	<!--BODY-->
 	<div class="mainbody">
-		<p><?php echo "Hello world!" ?></p>
+		<p>Hello world!</p>
 	</div>
 
 </body>
