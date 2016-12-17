db96c606d516042f1cf002451c5c306476debbce
antisochelle <delarosamchelle@gmail.com>
2016-12-17 14:49:19 +0000

    trying to connect to database

--- login.php
+++ login.php
@@ -80,7 +80,7 @@
 		$pg_conn = pg_connect(pg_connection_string_from_database_url());
 		# Now let's use the connection for something silly just to prove it works:
 		$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-		print "<p style="Montserrat;">\n";
+		print "<p>\n";
 		if (!pg_num_rows($result)) {
 		  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 		} else {
