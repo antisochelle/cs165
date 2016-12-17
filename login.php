1d42255a51187a8c03746ad68522e838e4f6870e
antisochelle <delarosamchelle@gmail.com>
2016-12-17 18:40:48 +0000

    format printing of products

--- login.php
+++ login.php
@@ -80,29 +80,12 @@
 			  extract(parse_url($_ENV["DATABASE_URL"]));
 			  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 			}
-			# Here we establish the connection. Yes, that's all.
-			$pg_conn = pg_connect(pg_connection_string_from_database_url());
-			# Now let's use the connection for something silly just to prove it works:
-			$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-			print "\n";
-			if (!pg_num_rows($result)) {
-			  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-			} else {
-			  print "<p>Tables in your database:</p>\n";
-			  print "<ul>\n";
-			  while ($row = pg_fetch_row($result)) { print("<li>$row[0]</li>\n"); }
-			}
-			print "</ul>\n";
 			
-			# Get list of userID
-			$userID = pg_query($pg_conn, "SELECT userID FROM Users");
-			print "<p> Users in your database:</p>\n";
-			print "<ul>";
-			while ($row = pg_fetch_row($userID)){
-				print("<li>$row[0]</li>");
-			}
-			print "</ul>";
+			# Establish connection
+			$pg_conn = pg_connect(pg_connection_string_from_database_url());
 		?>
+		
+		<br>
 		
 		<table>
 			<tr>
