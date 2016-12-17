ec5639e692571e9337962ccf8222acc24354427e
antisochelle <delarosamchelle@gmail.com>
2016-12-17 16:43:36 +0000

    printing results from user (testing)

--- login.php
+++ login.php
@@ -95,7 +95,7 @@
 		print "<p> Users in your database:</p>\n";
 		print "<ul>";
 		while ($row = pg_fetch_row($userID)){
-			print("<li>$row</li>");
+			print("<li>$row[0]</li>");
 		}
 		print "</ul>";
 		?>
