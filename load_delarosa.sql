6bc3eb9f72441dc3692954e681c607e9038c7245
antisochelle <delarosamchelle@gmail.com>
2016-12-17 06:00:08 +0000

    pushing back this directory

--- load_delarosa.sql
+++ load_delarosa.sql
@@ -30,5 +30,13 @@ INSERT INTO Products (productNumber, productName, productDescription, productSta
 INSERT INTO Products (productNumber, productName, productDescription, productStatus, productQuantity, productPrice) VALUES (5,'Motivation','Very in demand so order now!','Out of stock',0,25.00);
 
 /* Carts */
+INSERT INTO Carts (cartNumber, userID) VALUES (1, 'user1');
+INSERT INTO Carts (cartNumber, userID) VALUES (2, 'user2');
+INSERT INTO Carts (cartNumber, userID) VALUES (3, 'user3');
 
 /* Orders */
+INSERT INTO Orders (orderNumber, cartNumber, productNumber, orderQuantity, orderStatus, orderDate) VALUES (1,1,1,2,'NOT OK',2016-12-07 04:19:51.988843);
+INSERT INTO Orders (orderNumber, cartNumber, productNumber, orderQuantity, orderStatus, orderDate) VALUES (2,2,2,1,'NOT OK',2016-12-07 04:20:06.142867);
+INSERT INTO Orders (orderNumber, cartNumber, productNumber, orderQuantity, orderStatus, orderDate) VALUES (3,3,1,1,'NOT OK',2016-12-07 05:20:06.143485);
+INSERT INTO Orders (orderNumber, cartNumber, productNumber, orderQuantity, orderStatus, orderDate) VALUES (4,1,3,2,'OK',2016-12-07 05:20:07.100000);
+INSERT INTO Orders (orderNumber, cartNumber, productNumber, orderQuantity, orderStatus, orderDate) VALUES (5,2,3,1,'OK',2016-12-07 06:30:06.142867);
