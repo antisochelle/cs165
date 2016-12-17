d5d149be1fae5f69948fe11c06aa8eab3edb7aa1
antisochelle <delarosamchelle@gmail.com>
2016-12-17 19:55:32 +0000

    checking for valid input

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
 	<div class="top" style="position: fixed; top: 0px; left: 0px; right: 0px;">
 		<a href="https://cs165.herokuapp.com/" class="logo">B E S H I E</a>
 		
-		<form action="<?php echo $success; ?>" method="post" class="textbox" style="margin-right: 30px;">
+		<form action="<?php if ($userID = ""){ echo htmlspecialchars($_SERVER["PHP_SELF"]);} else { echo $success;} ?>" method="post" class="textbox" style="margin-right: 30px;">
     		<button class="submit" type="submit" formaction="signup.php">SIGN UP</button>
     		<input class="submit" type="submit" value="Shop Now" style="margin-right: 10px;">
     		<input class="textbox" type="password" name="userPassword" placeholder="Password">
@@ -115,6 +115,8 @@
 	<div class="mainbody">
 		<p><?php echo "Hello world!" ?></p>
 		<p>Enter login credentials!</p>
+		
+		
 	</div>
 
 </body>
