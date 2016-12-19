bd4519183d01b83046f146f7a58a6e203add28b1
antisochelle <delarosamchelle@gmail.com>
2016-12-19 14:52:07 +0000

    restrict add product to admin only

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
 <?php
    session_start();
    
+   # Remove all variables
+   session_unset();
+   
+   # Destroy session
+   session_destroy();
+   
    if(session_destroy()) {
-      header("Location: index.php");
+      header("location:https://cs165.herokuapp.com/");
    }
 ?>
