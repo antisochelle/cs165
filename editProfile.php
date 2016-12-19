7a0f247d309be04e0fd374e86c08a5ea003bfe16
antisochelle <delarosamchelle@gmail.com>
2016-12-19 07:28:51 +0000

    added Edit Profile

--- editProfile.php
+++ editProfile.php
@@ -1,2 +1,86 @@
-<p>Coming soon!</p>
-<p><a href="login.php">Go back</a></p>
+<?php
+	# Include connection to DB
+	include('config.php');
+
+	# Session
+	session_start();
+	
+	# Get userID
+	$userID = $_SESSION['login_user'];
+?>
+
+<!DOCTYPE html>
+<html>
+
+<title>CS 165 APP</title>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style type="text/css">
+	body {font-family: "Montserrat", sans-serif;}
+
+	.top {
+		background: black;
+		color: white;
+		max-height: 70px;
+		padding-left: 50px;
+		padding-top: 20px;
+		padding-right: 50px;
+		padding-bottom: 20px;
+		display: block;
+	}
+
+	.logo {
+		font-size: 25px;
+		color: white;
+		text-decoration: none;
+		margin: auto;
+	}
+
+	.submit {
+		font-family: "Montserrat", sans-serif;
+		font-size: 15px;
+		background-color: white;
+		color: black;
+		height: 21.5px;
+		min-width: 50px;
+		border: 0;
+		float: right;
+	}
+
+	.mainbody {
+		margin-left: 50px;
+		margin-top: 100px;
+	}
+
+</style>
+
+<body>
+	
+	<!--HEADER-->
+	<div class="top" style="position: fixed; top: 0px; left: 0px; right: 0px;">
+		<a href="https://cs165.herokuapp.com/" class="logo">B E S H I E</a>
+		<form action="index.php" method="post" class="submit" style="margin-top: 4px;">
+          <button type="submit" class="submit">LOG OUT</button>
+        </form>
+		
+	</div>
+
+	<!--BODY-->
+	<div class="mainbody">
+		
+	    <form method="post" action="">
+	        <p>New name:</p>
+	        <input type="text" name="newName" maxlength="50"><br>
+	        <p>New address:</p>
+	        <input type="text" name="newAddress" maxlength="100"><br>
+	        <input type="submit" value="UPDATE PROFILE">
+	    </form>
+	
+	</div>
+
+</body>
+
+</html>
